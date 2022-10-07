776588a80886f4d30db4807e3c98b4714c6c0536
Varshini <varshuk01@gmail.com>
2022-10-07 21:02:48 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdddonarComponent } from './adddonar/adddonar.component';
 import { SearchdonarComponent } from './searchdonar/searchdonar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdddonarComponent,
-    SearchdonarComponent
+    SearchdonarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
